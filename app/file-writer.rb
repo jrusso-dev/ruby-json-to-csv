22a5abc89ab2ccdc1676f0afb44dac74d6e07d29
Julien Russo <j.russo@magicstay.com>
2020-12-30 02:50:15 +0100

    Add some verifications (to be improved)

---
+++ app/file-writer.rb
@@ -0,0 +1,17 @@
+class FileWriter
+  attr_accessor :path, :file_handler
+
+  def initialize(path)
+    @path = path
+    @file_handler = File.open(path, 'w+')
+  end
+
+  def write(content)
+    @file_handler.write(content + "\n")
+  end
+
+  def close
+    @file_handler.close()
+  end
+
+end
--- app/json-to-csv.rb
+++ app/json-to-csv.rb
@@ -1,25 +1,45 @@
 require_relative 'json-parser'
 require_relative 'csv-line-converter'
+require_relative 'file-writer'
 
-input_path = "/workspace/ruby/rubytutorial/app/assets/users.json"
-output_path = "/workspace/ruby/rubytutorial/app/assets/users.csv"
-
-input_file = File.open(input_path,"r")
-input_file_content = input_file.read()
-input_file.close()
-
-json_parser = JsonParser.new(input_file_content)
-parsed_content = json_parser.parsed_content
-
-first_line = parsed_content[0]
-csv_line_converter = CsvLineConverter.new(first_line)
-header_csv = csv_line_converter.get_csv_header
-
-output_file = File.open(output_path, "w+")
-output_file.write(header_csv+"\n")
-for line in parsed_content
-  csv_line_converter = CsvLineConverter.new(line)
-  data_csv = csv_line_converter.get_csv_data
-  output_file.write(data_csv+"\n")
+def display_help
+  puts ('Usage :')
+  puts ('ruby json-to-csv.rb /path/to/input/json/file.json /path/to/output/csv/file.csv')
 end
-output_file.close()
+
+arguments_list = ARGV
+if arguments_list.length != 2
+  puts 'No argument specified'
+  display_help()
+else
+  input_path = arguments_list[0]
+  output_path = arguments_list[1]
+  json_file_exists = true
+
+  puts "Reading input JSON file #{input_path}"
+  begin
+    input_file = File.open(input_path,"r")
+  rescue
+    puts "Input JSON file doesn't exist !"
+    json_file_exists = false
+  end
+
+  if json_file_exists
+    parsed_content = JsonParser.new(input_file.read()).parsed_content
+    puts "Content parsed successfully"
+
+    first_line = parsed_content[0]
+    header_csv = CsvLineConverter.new(first_line).get_csv_header
+
+    file_writer = FileWriter.new(output_path)
+    file_writer.write(header_csv)
+    for line in parsed_content
+      data_csv = CsvLineConverter.new(line).get_csv_data
+      file_writer.write(data_csv)
+    end
+    puts "Writing output CSV file #{output_path}"
+
+    input_file.close()
+    file_writer.close()
+  end
+end
